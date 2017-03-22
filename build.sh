f2cbf88172b95f89d2606aade71a466526929dc0
Oskar Maria Grande <oskar@daruma.io>
2017-03-22 17:05:31 +0100

    Add boot-clj docker image build steps

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t scratch .
+docker build -t scratch-gui .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 8601:8601 -it scratch
+docker run -p 8601:8601 -it scratch-gui
